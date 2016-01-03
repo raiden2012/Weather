d4c5072aab7421a56168fcf406e91f7773c27a34
Wei Tu <tuwei18@gmail.com>
2016-01-03 22:52:25 +0800

    Initial Commit

---
+++ Weather/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Weather
+//
+//  Created by Wei Tu on 3/1/2016.
+//  Copyright © 2016 Wei Tu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Weather/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Weather
+//
+//  Created by Wei Tu on 3/1/2016.
+//  Copyright © 2016 Wei Tu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
